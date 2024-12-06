8c08cc139178bd941e0697eed8997c9613adf4a8
Gianmario Iamoni <gianmario.iamoni@gmail.com>
2024-12-07 00:32:11 +0100

    small UI changes

--- client/src/components/UserBreadcrumbs.tsx
+++ client/src/components/UserBreadcrumbs.tsx
@@ -12,24 +12,11 @@ const UserBreadcrumbs: FC<UserBreadcrumbsProps> = ({userDetails}) => {
     return (
         <div className="mb-4">
             <Breadcrumbs>
-                <BreadcrumbItem href="/home" className="hover:primary">Home</BreadcrumbItem>
+                <BreadcrumbItem href="/" className="hover:primary">Home</BreadcrumbItem>
                 <BreadcrumbItem href="/users" className="hover:primary">Utenti</BreadcrumbItem>
                 <BreadcrumbItem>{userDetails.nome} {userDetails.cognome}</BreadcrumbItem>
             </Breadcrumbs>
         </div>
-        // <nav className="mb-4">
-        //     <ol className="list-reset flex text-sm text-gray-600">
-        //         <li>
-        //             <Link href="/" className="hover:primary">Home</Link>
-        //             <span className="mx-2">/</span>
-        //         </li>
-        //         <li>
-        //             <Link href="/users" className="hover:primary">Users</Link>
-        //             <span className="mx-2">/</span>
-        //         </li>
-        //         <li className="text-foreground font-semibold">{userDetails.nome} {userDetails.cognome}</li>
-        //     </ol>
-        // </nav>
     );
 };
 
