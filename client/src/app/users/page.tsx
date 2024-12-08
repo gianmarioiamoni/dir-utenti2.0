20a1df1a68a9e4ae7dcc15a05e4976954199c0ba
Gianmario Iamoni <gianmario.iamoni@gmail.com>
2024-12-08 23:52:35 +0100

    add loader

--- client/src/app/users/page.tsx
+++ client/src/app/users/page.tsx
@@ -5,6 +5,7 @@ import { useUsers } from '@/hooks/useUsers';
 import UserCard from '@/components/UserCard';
 import Pagination from '@/components/Pagination';
 import AddUserDialog from '@/components/AddUserDialog';
+import Loader from '@/components/Loader';
 
 
 const UsersPage: React.FC = () => {
@@ -20,44 +21,56 @@ const UsersPage: React.FC = () => {
         setIsModalOpen(false);
     };
 
-    if (isLoading) return <div className="text-center p-4">Caricamento...</div>;
+    // if (isLoading) return <div className="text-center p-4">Caricamento...</div>;
     if (isError) return <div className="text-center p-4 text-red-500">Errore nel caricamento degli utenti</div>;
 
     return (
         <div className="container flex flex-col min-h-screen px-8 py-8 bg-background text-foreground">
-            <div className="flex justify-between items-center mb-6">
-                <h1 className="text-3xl font-bold">Lista Utenti</h1>
-                <button
-                    className="btn-primary"
-                    onClick={() => setIsModalOpen(true)}
-                >
-                    Aggiungi utente
-                </button>
-            </div>
 
             {/* Modale */}
             <AddUserDialog isOpen={isModalOpen} onClose={onClose} />
 
-            <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-4">
-                {data?.users.map((user) => (
-                    <UserCard
-                        key={user._id}
-                        _id={user._id}
-                        nome={user.nome}
-                        cognome={user.cognome}
-                        email={user.email}
-                    />
-                ))}
-            </div>
-
-            {data?.total && (
-                <div className="mt-20 flex justify-center">
-                    <Pagination
-                        currentPage={currentPage}
-                        totalPages={Math.ceil(data.total / 10)}
-                        onPageChange={handlePageChange}
-                    />
+            {/* Mostra il loader o lista utenti */}
+            {isLoading ? (
+                <div className="flex justify-center items-center h-32">
+                    <Loader isLoading={isLoading} />
                 </div>
+            ) : (
+                <>
+                    <div className="flex justify-between items-center mb-6">
+                        <h1 className="text-3xl font-bold">Lista Utenti</h1>
+                        <button
+                            className="btn-primary"
+                            onClick={() => setIsModalOpen(true)}
+                        >
+                            Aggiungi utente
+                        </button>
+                    </div>
+                    {/* Lista utenti */}
+                    <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-4">
+                        {data?.users.map((user) => (
+                            <UserCard
+                                key={user._id}
+                                _id={user._id}
+                                nome={user.nome}
+                                cognome={user.cognome}
+                                email={user.email}
+                            />
+                        ))}
+                    </div>
+
+                    {/* Pagination */}
+                    {data?.total && (
+                        <div className="mt-20 flex justify-center">
+                            <Pagination
+                                currentPage={currentPage}
+                                totalPages={Math.ceil(data.total / 10)}
+                                onPageChange={handlePageChange}
+                            />
+                        </div>
+
+                    )}
+                </>
             )}
         </div>
     );
---
+++ client/src/components/Loader.tsx
@@ -0,0 +1,33 @@
+import { FC } from "react";
+
+/**
+ * Proprietà del componente `Loader`.
+ *
+ * @typedef {Object} LoaderProps
+ * @property {boolean} isLoading - Indica se il caricamento è in corso.
+ */
+interface LoaderProps {
+    isLoading: boolean;
+}
+
+/**
+ * Componente per visualizzare un'animazione di caricamento.
+ *
+ * @component
+ * @param {LoaderProps} props - Proprietà passate al componente.
+ * @returns {JSX.Element | null} - Il componente di caricamento se `isLoading` è `true`, altrimenti `null`.
+ *
+ * @example
+ * <Loader isLoading={true} />
+ */
+const Loader: FC<LoaderProps> = ({ isLoading }: LoaderProps) => {
+    return isLoading ? (
+        <div className="flex flex-col items-center justify-center loader-container">
+            {/* Animazione caricamento */}
+            <div className="loader" />
+            <p className="loader-text">Caricamento Utenti...</p>
+        </div>
+    ) : null;
+};
+
+export default Loader;
