c675d4aaa8914f5558f19445a5521ee877f6d41d
Gianmario Iamoni <gianmario.iamoni@gmail.com>
2024-12-14 19:10:49 +0100

    fix image loading in development

--- client/next.config.js
+++ client/next.config.js
@@ -1,12 +1,29 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
-    domains: ['res.cloudinary.com'], // Allow Cloudinary domain
+    domains: ['avatars.githubusercontent.com', 'res.cloudinary.com', 'localhost', '127.0.0.1'],
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'avatars.githubusercontent.com',
+      },
+      {
+        protocol: 'https',
+        hostname: 'res.cloudinary.com',
+      },
+      {
+        protocol: 'http',
+        hostname: 'localhost',
+        port: '3000',
+      },
+      {
+        protocol: 'http',
+        hostname: '127.0.0.1',
+        port: '3000',
+      }
+    ],
   },
-  // Enable static exports for production
-  output: 'standalone',
-  // Enable strict mode in development
   reactStrictMode: true,
-}
+};
 
-module.exports = nextConfig
+export default nextConfig;
